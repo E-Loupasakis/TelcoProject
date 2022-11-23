0335769354f6354c6da8a9cc0f00c45533bbc210
LoupasMa <manos.loupas@gmail.com>
2022-11-23 16:15:25 +0200

    1st commit

---
+++ src/main/java/gr/codehub/telco/telcoproject/HelloApplication.java
@@ -0,0 +1,9 @@
+package gr.codehub.telco.telcoproject;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
