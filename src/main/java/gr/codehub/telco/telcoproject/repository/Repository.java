97bc8ae79ec0193034d20ce14d7627485ab09c0c
KefalaNi <nikos.kefalas@outlook.com>
2022-12-01 16:03:12 +0200

    commit changes in ticket and repo of ticket

---
+++ src/main/java/gr/codehub/telco/telcoproject/repository/Repository.java
@@ -0,0 +1,12 @@
+package gr.codehub.telco.telcoproject.repository;
+
+import java.util.List;
+
+public interface Repository<T, K> {
+    //CR(U)D
+    T create(T t);
+    T read(K id);
+    List<T> read();
+    boolean delete(K id);
+    T update(T t);
+}
---
+++ src/main/java/gr/codehub/telco/telcoproject/repository/TicketRepository.java
@@ -0,0 +1,7 @@
+package gr.codehub.telco.telcoproject.repository;
+
+import gr.codehub.telco.telcoproject.model.Ticket;
+
+public interface TicketRepository extends Repository<Ticket, Long> {
+
+}
