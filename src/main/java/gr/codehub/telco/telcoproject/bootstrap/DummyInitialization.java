e68b667821f6b48d312e48266629d241ac396a01
KefalaNi <nikos.kefalas@outlook.com>
2022-12-02 15:48:19 +0200

    Find tickets by customer id

--- src/main/java/gr/codehub/telco/telcoproject/bootstrap/DummyInitialization.java
+++ src/main/java/gr/codehub/telco/telcoproject/bootstrap/DummyInitialization.java
@@ -4,6 +4,7 @@ package gr.codehub.telco.telcoproject.bootstrap;
 import gr.codehub.telco.telcoproject.enums.TicketStatus;
 import gr.codehub.telco.telcoproject.model.Ticket;
 import gr.codehub.telco.telcoproject.model.User;
+import gr.codehub.telco.telcoproject.repository.TicketRepository;
 import gr.codehub.telco.telcoproject.repository.impl.CustomerRepositoryImpl;
 import gr.codehub.telco.telcoproject.repository.impl.TicketRepositoryImpl;
 import jakarta.annotation.PostConstruct;
@@ -12,6 +13,7 @@ import jakarta.ejb.Startup;
 import jakarta.inject.Inject;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Singleton
 @Startup
@@ -25,16 +27,59 @@ public class DummyInitialization {
     @PostConstruct
     public void test() {
         User us1 = User.builder().lastName("nn").lastName("Kef").emailAddress("nn@gmail.com").build();
+        User us2 = User.builder().lastName("Kef").emailAddress("nn@gmail.com").firstName("Kostas").build();
         System.out.println(us1);
         cstimpl.create(us1);
-        Ticket ticket= Ticket.builder().ticketStatus(TicketStatus.IN_PROGRESS).addressOfIssue("Zallogou 9").dateTimeOfAction(LocalDateTime.now().plusMonths(1))
+        cstimpl.create(us2);
+        Ticket ticket1 = Ticket.builder().ticketStatus(TicketStatus.IN_PROGRESS).addressOfIssue("Zallogou 9").dateTimeOfAction(LocalDateTime.now().plusMonths(1))
                 .description("Digital").estimatedCost(34.2).dateTimeOfCreation(LocalDateTime.now()).customer(us1).build();
 
+        Ticket ticket2 = Ticket.builder().ticketStatus(TicketStatus.PENDING).addressOfIssue("Alimoy 9").dateTimeOfAction(LocalDateTime.now().plusMonths(6))
+                .description("Telephone").estimatedCost(24.87).dateTimeOfCreation(LocalDateTime.now()).customer(us1).build();
 
-        System.out.println(ticket);
-       ticketRepositoryImpl.create(ticket);
-        ticket.setAddressOfIssue("Zallogou 8");
-        ticketRepositoryImpl.update(ticket);
+        Ticket ticket3 = Ticket.builder().ticketStatus(TicketStatus.DEFAULT_STANDBY_MODE).addressOfIssue("Zografoy 9").dateTimeOfAction(LocalDateTime.now().plusMonths(3))
+                .description("Internet").estimatedCost(50).dateTimeOfCreation(LocalDateTime.now()).customer(us1).build();
+
+        Ticket ticket10 = Ticket.builder().ticketStatus(TicketStatus.COMPLETE).addressOfIssue("Ioannoy 9").dateTimeOfAction(LocalDateTime.now().plusMonths(5))
+                .description("Ethernet").estimatedCost(1_000_000).dateTimeOfCreation(LocalDateTime.now()).customer(us2).build();
+
+
+
+        ticketRepositoryImpl.create(ticket1);
+        ticketRepositoryImpl.create(ticket2);
+        ticketRepositoryImpl.create(ticket3);
+
+
+        List<Ticket> tickets = ticketRepositoryImpl.read();
+        for (Ticket ticket : tickets
+             ) {
+            System.out.println(ticket);
+        }
+
+        //ticketRepositoryImpl.delete(ticket1.getTicketId());
+
+        Ticket ticketRead = ticketRepositoryImpl.read(ticket2.getTicketId());
+
+        System.out.println("ticketRead ========> " + ticketRead);
+
+        System.out.println(tickets);
+
+        ticketRead.setDescription("UPDATE");
+
+        ticketRepositoryImpl.update(ticketRead);
+
+        List<Ticket> ticketsByCustomerId = ticketRepositoryImpl.getTicketsByCustomerId(us1.getUserId());
+
+        System.out.println("====================TICKETS BY CUSTOMER ID=================================");
+
+        for (Ticket ticket : ticketsByCustomerId
+        ) {
+            System.out.println(ticket);
+        }
+
+
+
+
     }
 
 }
--- src/main/java/gr/codehub/telco/telcoproject/repository/impl/TicketRepositoryImpl.java
+++ src/main/java/gr/codehub/telco/telcoproject/repository/impl/TicketRepositoryImpl.java
@@ -2,6 +2,11 @@ package gr.codehub.telco.telcoproject.repository.impl;
 
 import gr.codehub.telco.telcoproject.model.Ticket;
 import gr.codehub.telco.telcoproject.repository.TicketRepository;
+import jakarta.persistence.Query;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class TicketRepositoryImpl extends RepositoryImpl<Ticket, Long> implements TicketRepository {
     @Override
@@ -13,4 +18,24 @@ public class TicketRepositoryImpl extends RepositoryImpl<Ticket, Long> implement
     public String getClassName() {
         return Ticket.class.getSimpleName();
     }
+
+    public List<Ticket> getTicketsByCustomerId(Long customerId) {
+        Query query = super.getEm().createQuery("select t from "+ getClassName()+ " t WHERE t.customer IN (SELECT DISTINCT u.userId FROM User u WHERE u.userId IN (?1)) " );
+
+        query.setParameter(1, customerId);
+
+        List<Ticket> tickets = query.getResultList();
+
+        return tickets;
+    }
+
+   /* public List<Ticket> getTicketsByDate(LocalDateTime localDateTime) {
+        Query query = super.getEm().createQuery("select t from "+ getClassName()+ " t WHERE t.dateTimeOfCreation IN (?1)) " );
+
+        query.setParameter(1, localDateTime);
+
+        List<Ticket> tickets = query.getResultList();
+
+        return tickets;
+    }*/
 }
