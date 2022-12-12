416b80792dcbf5325d0ed8ee86afe15322923c2a
KefalaNi <nikos.kefalas@outlook.com>
2022-12-12 15:55:32 +0200

    InvalidFormatExceptionHandler

---
+++ src/main/java/gr/codehub/telco/telcoproject/exception/InvalidFormatExceptionHandler.java
@@ -0,0 +1,32 @@
+package gr.codehub.telco.telcoproject.exception;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import gr.codehub.telco.telcoproject.enums.TicketStatus;
+import gr.codehub.telco.telcoproject.transfer.ApiError;
+import gr.codehub.telco.telcoproject.transfer.ApiResponse;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Provider
+public class InvalidFormatExceptionHandler implements ExceptionMapper<InvalidFormatException> {
+
+    @Override
+    public Response toResponse(InvalidFormatException exception) {
+        log.info("Test placeholder {}", LocalDateTime.now());
+        Response.Status response = Response.Status.BAD_REQUEST;
+        return Response
+                .status(response)
+                .entity(
+                        ApiResponse.builder().apiError(
+                                        new ApiError(response.getStatusCode(),exception.getMessage())
+                                )
+                                .build()
+
+                ).build();
+    }
+}
