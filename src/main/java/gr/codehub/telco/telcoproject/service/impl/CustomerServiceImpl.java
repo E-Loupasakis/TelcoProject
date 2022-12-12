843f5ab099368a547db2839f751c9a55e1276e79
LoupasMa <manos.loupas@gmail.com>
2022-12-12 10:56:08 +0200

    checks for user creation completed

--- src/main/java/gr/codehub/telco/telcoproject/service/impl/CustomerServiceImpl.java
+++ src/main/java/gr/codehub/telco/telcoproject/service/impl/CustomerServiceImpl.java
@@ -1,16 +1,13 @@
 package gr.codehub.telco.telcoproject.service.impl;
 
 import gr.codehub.telco.telcoproject.exception.CustomerPropertiesExistingException;
-import gr.codehub.telco.telcoproject.dto.CustomerDto;
 import gr.codehub.telco.telcoproject.exception.UserNameExists;
-import gr.codehub.telco.telcoproject.model.Email;
 import gr.codehub.telco.telcoproject.model.Ticket;
 import gr.codehub.telco.telcoproject.model.User;
 import gr.codehub.telco.telcoproject.repository.CustomerRepository;
 import gr.codehub.telco.telcoproject.service.CustomerService;
 import jakarta.inject.Inject;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
@@ -24,20 +21,9 @@ public class CustomerServiceImpl implements CustomerService {
         if(!exceptionMessage.isEmpty()){
             throw new CustomerPropertiesExistingException( exceptionMessage );
         }
-        customer.getEmailList().forEach( (email) -> {
-            if(!(customerRepository.getCustomerByEmail(email.getEmail()).isEmpty())){
-                    throw new CustomerPropertiesExistingException("Email exists");
-            }
-        });
-
-        if(customerRepository.getCustomerByUserName(customer.getUsername())!=null){
-            throw new UserNameExists("Username exists");
-        }
 
         return customerRepository.create(customer);
     }
-
-
 
 
     @Override
@@ -90,28 +76,26 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public List<Ticket> findTicketsByCustomerId(long customerId) {
         return  customerRepository.read(customerId).getTickets();
-
     }
 
     private String checkProperties(User customer) {
         StringBuilder exceptionMessage = new StringBuilder();
         if(customerRepository.getCustomerByVat(customer.getVatNumber())!=null){
             exceptionMessage.append("Vat exists\n");
-            //throw new VatExistsException("Vat exists");
         }
         customer.getEmailList().forEach( (email) -> {
             if(!(customerRepository.getCustomerByEmail(email.getEmail()).isEmpty())){
                 exceptionMessage.append("Email "+email.getEmail()+" is already in use.\n");
-                //throw new EmailExistsException("Email exists");
             }
         });
         customer.getPhones().forEach( (phone) -> {
             if(!(customerRepository.getCustomerByTelephone(phone.getNumber()).isEmpty())){
                 exceptionMessage.append("Phone "+phone.getNumber()+" is already in use.\n");
-                //throw new EmailExistsException("Phone exists");
             }
         });
-
+        if(customerRepository.getCustomerByUserName(customer.getUsername())!=null){
+            exceptionMessage.append("Username is already in use.\n");
+        }
         return exceptionMessage.toString();
     }
 }
