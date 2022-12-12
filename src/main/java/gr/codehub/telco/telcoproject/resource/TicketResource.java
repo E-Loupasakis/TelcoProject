dd4a6f2107ad53de1b196842ce49628a3cd56f98
KefalaNi <nikos.kefalas@outlook.com>
2022-12-12 18:30:12 +0200

    Added Response in Ticket Resource

--- src/main/java/gr/codehub/telco/telcoproject/resource/TicketResource.java
+++ src/main/java/gr/codehub/telco/telcoproject/resource/TicketResource.java
@@ -40,7 +40,7 @@ public class TicketResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response insert(@Valid Ticket ticket) {
         ticket.setDateTimeOfCreation(LocalDateTime.now());
-       // return ticketService.create(ticket);
+       //return ticketService.create(ticket);
         return Response.ok().entity(ApiResponse.builder().data(ticketService.create(ticket)).build()).build();
     }
 
@@ -59,51 +59,53 @@ public class TicketResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response findByTicketId(@PathParam("id") Long id) {
         ticketService.findByTicketId(id);
-        return Response.noContent().entity(ApiResponse.builder().build()).build();
+        return Response.ok().entity(ApiResponse.builder().data(ticketService.findByTicketId(id)).build()).build();
     }
 
     @PUT
     @Path("/{ticketId}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Ticket update(@PathParam("ticketId") long ticketId, @Valid Ticket ticket) {
+    public Response update(@PathParam("ticketId") long ticketId, @Valid Ticket ticket) {
         ticket.setTicketId(ticketId);
-        return ticketService.update(ticket);
+        ticketService.update(ticket);
+        return Response.ok().entity(ApiResponse.builder().build()).build();
     }
 
     @DELETE
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public boolean delete(@PathParam("id") Long id) {
-        return ticketService.delete(id);
+    public Response delete(@PathParam("id") Long id) {
+        return Response.ok().entity(ApiResponse.builder().data(ticketService.delete(id)).build()).build();
     }
+
 
     @GET
     @Path("/search-by-dates/{dateFrom}&{dateTo}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Ticket> findByDateRange(@PathParam("dateFrom") String dateFrom,@PathParam("dateTo")  String  dateTo) {
+    public Response findByDateRange(@PathParam("dateFrom") String dateFrom,@PathParam("dateTo")  String  dateTo) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
         LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
-        return ticketService.findByDateRange(localDateFrom, localDateTo);
+        return  Response.ok().entity(ApiResponse.builder().data(ticketService.findByDateRange(localDateFrom, localDateTo)).build()).build();
     }
 
     @GET
     @Path("/search-by-date/{date}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Ticket> findByDate(@PathParam("date") String date) {
+    public Response findByDate(@PathParam("date") String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate localDate = LocalDate.parse(date, formatter);
-        return ticketService.findByDate(localDate);
+        return  Response.ok().entity(ApiResponse.builder().data(ticketService.findByDate(localDate)).build()).build();
     }
 
     @GET
     @Path("/search-by-customer-id/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public List<Ticket> findByCustomerId(@PathParam("id") Long id) {
-        return ticketService.getTicketsByCustomerId(id);
+    public Response findByCustomerId(@PathParam("id") Long id) {
+        return  Response.ok().entity(ApiResponse.builder().data(ticketService.getTicketsByCustomerId(id)).build()).build();
     }
 
 
